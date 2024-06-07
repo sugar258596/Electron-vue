7f5a1808aca1a9422b71db9470e8f15e80635476
sugar258956 <1260698917@qq.com>
2024-06-08 05:32:29 +0800

    feat:UnoCss 配置指令

--- uno.config.ts
+++ uno.config.ts
@@ -12,12 +12,35 @@ import {
 
 export default defineConfig({
   shortcuts: [
-    // ...
+    {
+      'flex-center': 'flex justify-center items-center',
+      full: 'w-full h-full'
+    },
+    [/^grid-(\d+)-(\d+)$/, ([, c, g = 0]) => `grid grid-cols-${c}  gap-${g}`],
+    [/^img-(.*)$/, ([, c]) => `full object-${c}`],
+    [/^ma-(\d+)-(\d+)$/, ([, i, b]) => `mx-${i} my-${b}`],
+    [/^pa-(\d+)-(\d+)$/, ([, i, b]) => `px-${i} py-${b}`],
+    [/^bor-(\d+)-(.*)$/, ([, b, c]) => `b-${b} b-solid b-${c}`],
+    [/^bor-rd-(.*)$/, ([, b]) => `b-rd-${b} overflow-hidden`],
+    [/^border-([tblr])-(\d+)-(.*)$/, ([, t, b, c]) => `b-${t}-${b} b-${t}-solid b-${t}-${c}`],
+    [/^t-(.*)$/, ([, s]) => `text-${s} color-zinc-500 `],
+    [/^wh-(.*)$/, ([, n]) => `h-${n} w-${n}`],
+    [/^text-(.*)-imp/, ([, s]) => `text-${s}!`]
+  ],
+  rules: [
+    [
+      'page',
+      {
+        width: '100%',
+        height: '100%',
+        'min-height': '100vh'
+      }
+    ],
+    ['bg-linear', { background: 'linear-gradient(to right,#9f9fd9, pink)' }]
   ],
   theme: {
-    colors: {
-      // ...
-    }
+    // 解决小程序不支持 * 选择器
+    preflightRoot: ['page,::before,::after']
   },
   presets: [
     presetUno(),
@@ -30,5 +53,6 @@ export default defineConfig({
       }
     })
   ],
-  transformers: [transformerDirectives(), transformerVariantGroup()]
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+  variants: []
 })
