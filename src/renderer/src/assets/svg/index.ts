33a1e361059fdf93253e051fdd2ee24ce29da8bd
sugar258956 <1260698917@qq.com>
2024-06-18 04:54:42 +0800

    wip：添加svg图标

--- src/renderer/src/assets/svg/index.ts
+++ src/renderer/src/assets/svg/index.ts
@@ -1,5 +1,7 @@
 import blbl from './blbl.svg' //  哔哩哔哩 图标
 import reset from './reset.svg' // 重置 图标
 import up from './up.svg' // up主 图标
+import bofan from './bofan.svg' // 播放 图标
+import xiaoxi from './xiaoxi.svg' // 消息 图标
 
-export { blbl, reset, up }
+export { blbl, reset, up, bofan, xiaoxi }
