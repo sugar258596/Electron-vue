0f1fe2f9b07300a47c21c9adf75133e3d7f75f11
sugar258956 <1260698917@qq.com>
2024-06-18 04:47:54 +0800

    feat: 添加svg 组件

---
+++ src/renderer/src/components/Svg/index.ts
@@ -0,0 +1,5 @@
+import baseSvg from './src/index.vue'
+
+import { withInstall } from '@/utils'
+
+export const BaseSvg = withInstall(baseSvg)
---
+++ src/renderer/src/components/Svg/src/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    class="image-contain cursor-pointer"
+    v-bind="$attrs"
+    loading="lazy"
+    :src="src"
+    :alt="alt"
+    srcset=""
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  src: string
+  alt?: string
+}>()
+</script>
+<style lang="scss" scoped></style>
