59a163440eabaa2239c0a143761a3b94eae081de
sugar258956 <1260698917@qq.com>
2024-07-10 01:08:45 +0800

    feat:添加字体图标组件

--- src/renderer/src/components/Svg/index.ts
+++ src/renderer/src/components/Svg/index.ts
@@ -1,5 +1,7 @@
 import baseSvg from './src/index.vue'
-
+import baseIcon from './src/BeseSvg.vue'
 import { withInstall } from '@/utils'
 
 export const BaseSvg = withInstall(baseSvg)
+
+export const BaseIcon = withInstall(baseIcon)
---
+++ src/renderer/src/components/Svg/src/BeseSvg.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="full flex-center p-1 bg-light-400 hover:bg-stone-200 cursor-pointer   bor-rd-10">
+    <div class="text-align-center m-a p-x-0.5">
+      <span class="anticon">
+        <svg class="icon" aria-hidden="true" v-bind="$attrs">
+          <use :href="`#${icon}`" :fill="color"></use>
+        </svg>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import '@/assets/iconfont/iconfont.js'
+
+defineProps({
+  icon: {
+    type: String,
+    default: ''
+  },
+  color: {
+    type: String,
+    default: 'rgba(0, 0, 0, 0.88)'
+  }
+})
+
+</script>
+<style scoped lang="scss">
+.anticon {
+  display: inline-flex;
+  align-items: center;
+  color: inherit;
+  font-style: normal;
+  line-height: 0;
+  text-align: center;
+  text-transform: none;
+  vertical-align: -0.125em;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
+<style scoped type="text/css">
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
